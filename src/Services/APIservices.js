3fef2b20351f631ab1afb91218b4ade7922f556f
karthick <karthickkavin004@gmail.com>
2024-03-26 12:48:00 +0530

    commit

--- src/Services/APIservices.js
+++ src/Services/APIservices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://urlshortener-backend-tmvd.onrender.com";
 // const BASE_URL = "https://url-backend-z3ta.onrender.com";
 
 export const addUser = async (payload) => {
